f3e61ab1f2eb6f315c60e7f855d4fff2c2dd3772
anton <onoshinanton@gmail.com>
2023-01-07 14:28:31 -0800

    main method created for first and second class

--- src/FIrst_Package/FirstClass.java
+++ src/FIrst_Package/FirstClass.java
@@ -1,4 +1,15 @@
 package FIrst_Package;
 
 public class FirstClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("First Class");
+
+
+    }
+
+
+
+
 }
--- src/FIrst_Package/SecondClass.java
+++ src/FIrst_Package/SecondClass.java
@@ -1,4 +1,11 @@
 package FIrst_Package;
 
 public class SecondClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("Second Class");
+
+
+    }
 }
